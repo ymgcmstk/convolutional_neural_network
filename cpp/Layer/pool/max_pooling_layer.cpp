1632488c6473a4a61f806542b3fc9bcb0710fcf4
ymgcmstk <yamaguchi@mi.t.u-tokyo.ac.jp>
2015-03-18 20:23:10 +0900

    copy conv_layer.cpp as max_pooling_layer.cpp

---
+++ cpp/Layer/pool/max_pooling_layer.cpp
@@ -0,0 +1,68 @@
+class MaxPoolingLayer : public BaseLayer {
+public:
+  MaxPoolingLayer () {
+    layer_name = "MaxPoolingLayer";
+  }
+  int kernel_x, kernel_y, stride, pad_x, pad_y;
+  int k_half, k_center;
+  float dropout;//たぶん使わない
+  void initialize() {
+    k_half = kernel_x / 2;
+    k_center = (kernel_x - 1) / 2;
+  }
+  bool forward () {
+    if (next_layer == NULL) return false;
+    if (kernel_x != kernel_y) {
+      cout << "conv_layer 17" << endl;
+      exit(1);
+      //あとで直そう
+    }
+    data = MatrixXf::Zero(x * y, z);
+    for (int i = 0; i < kernel_y; i++) {
+      for (int j = 0; j < kernel_x; j++) {
+        temp_mat = weights.block(0, prev_layer->z * (j + i * kernel_y), z, prev_layer->z).transpose();
+        to_slide = prev_layer->data * temp_mat;
+        data += slide(to_slide, - j + k_half, - i + k_half, y);
+      }
+    }
+    //bias
+    for (int i = 0; i < x * y; i++) data.block(i, 0, 1, z) += bias;
+    debug();
+    return true;
+  }
+
+  bool backward () {
+    if (prev_layer == NULL) return false;
+    prev_layer->influence = MatrixXf::Zero(prev_layer->x * prev_layer->y, prev_layer->z);
+    for (int i = 0; i < kernel_y; i++) {
+      for (int j = 0; j < kernel_x; j++) {
+        //diff計算
+        temp_mat = weights.block(0, prev_layer->z * (j + i * kernel_y), z, prev_layer->z);
+        to_slide = influence * temp_mat;
+        prev_layer->influence += slide(to_slide, j - k_half, i - k_half, y);
+
+        //重みの更新
+        temp_mat = slide(influence, j - k_half, i - k_half, y).transpose();
+        weights.block(0, prev_layer->z * (j + i * kernel_y), z, prev_layer->z) -= temp_mat * prev_layer->data;
+      }
+    }
+    //bias
+    bias -= MatrixXf::Ones(1, x * y) * influence;
+    debug();
+    return true;
+  }
+
+private:
+  MatrixXf output, temp_mat, to_slide;
+  MatrixXf slide(MatrixXf target, int slide_x, int slide_y, int this_y) {
+    output = MatrixXf::Zero(target.rows(), target.cols());
+    for (int i = 0; i < this_y; i++) {
+      if (i < -slide_y || i >= this_y - slide_y) {
+        continue;
+      }
+      output.block((i + slide_y) * this_y + max(0, slide_x), 0, this_y - abs(slide_x), target.cols()) =
+        target.block(i * this_y - min(0, slide_x), 0, this_y - abs(slide_x), target.cols());
+    }
+    return output;
+  }
+};
